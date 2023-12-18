1612448ecfa5ee3ab8eacfd5af847b55a21e57f0
chris-daou <chris.daou@lau.edu>
2023-12-19 01:28:55 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GenreMenuComponent } from './genre-menu/genre-menu.component';
+import { FilmsComponent } from './films/films.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, GenreMenuComponent, FilmsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/genre-menu/genre-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './genre-menu.component.html',
+  styleUrl: './genre-menu.component.css'
+})
+export class GenreMenuComponent {
+
+}
